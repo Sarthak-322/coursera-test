2902673e7536f7249251f77d062655ec4ff00cce
SARTHAK SRIVASTAVA <sssrivastava1999@gmail.com>
2022-04-30 19:34:55 +0530

    Small addition

--- module4/SpeakHello.js
+++ module4/SpeakHello.js
@@ -1,9 +1,8 @@
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
+(function(window){
+    var speakWord = "Hello";
+    var helloSpeaker = {};
+    helloSpeaker.speak = function (name){
+      console.log(speakWord + " " + name)
+    }
+    window.helloSpeaker = helloSpeaker;
 })(window);
